9996360a29319edee7a64589375d8e43324eedc9
Alexander Borgese <Alexander.Borgese@Gamesys.co.uk>
2019-05-04 15:43:09 +0100

    Updates

---
+++ precache-manifest.cecfa551327537f9daf49cc37b6a85c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/personal-website/static/media/gitHub.cefc2023.png"
+  },
+  {
+    "revision": "57e671561b358aa44ca131e111221e03",
+    "url": "/personal-website/static/media/AlexBorgeseCV.57e67156.pdf"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "78b1d2c5acdf4228b35e",
+    "url": "/personal-website/static/js/main.4aef8fa8.chunk.js"
+  },
+  {
+    "revision": "14a88121b3a4cf045caf",
+    "url": "/personal-website/static/js/2.6211ce1c.chunk.js"
+  },
+  {
+    "revision": "78b1d2c5acdf4228b35e",
+    "url": "/personal-website/static/css/main.876193e9.chunk.css"
+  },
+  {
+    "revision": "d6968b102dc07abe1e345abc93ee1f97",
+    "url": "/personal-website/index.html"
+  }
+];
